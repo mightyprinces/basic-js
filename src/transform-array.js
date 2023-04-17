7c4806f3347f448aa1d15dab90b7de8872d61e45
Mightyprincess <happyrabbit62@gmail.com>
2023-04-17 17:37:41 +0800

    feat: solve Transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -3,20 +3,48 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  let newArr = arr.slice(0);
+  for (let i = 0; i < newArr.length; i++) {
+    if (newArr[i] === '--discard-next') {
+      newArr[i+1] = null;
+    }
+
+    if (newArr[i] === '--discard-prev' && newArr[i-1] !== null) {
+      newArr[i-1] = null;
+    }
+
+    if (newArr[i] === '--double-next') {
+      newArr.splice(i+1, 0, newArr[i+1]);
+    }
+
+    if (newArr[i] === '--double-prev' && newArr[i-1] !== null) {
+      newArr.splice(i, 0, newArr[i-1]);
+      i++;
+    }
+  }
+
+  return newArr.filter(a => {
+    if (a !== null && a !== '--discard-next' && a !== '--discard-prev' && a !== '--double-next' && a !== '--double-prev') {
+      return a;
+    }
+  });
 }
+
 
 module.exports = {
   transform
